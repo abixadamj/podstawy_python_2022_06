24a6f54aeb3e21fe497f7838ba7e1618cf0b764d
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-06-21 10:56:14 +0200

    changes in avg_income

--- other_functions/finanse_01.py
+++ other_functions/finanse_01.py
@@ -1,12 +1,15 @@
-def avg_income(income, bonus=1.05):
+def avg_income(income, bonus=1.05, yearly_bonus=1.10):
+    """Our function compute next payment based on some criteria"""
     avg = sum(income) / len(income)
     next_income = avg * bonus
+    if len(income) >= 5:
+        next_income = next_income * yearly_bonus
     return next_income
 
 
-adam_income = [200, 400, 132, 34]
+adam_income = [200, 400, 132, 34, 300, 230]
 beata_income = [12.5, 33.6, 50]
 bonus = 3.02
 
 print(f"Next income for Adam: {avg_income(adam_income, 1.50)}")
-print(f"Next income for Adam: {avg_income(adam_income, bonus)}")
+print(f"Next income for Adam: {avg_income(beata_income, bonus)}")
--- payments.py
+++ payments.py
@@ -1,4 +1,5 @@
 from payment_functions.read_data import *
+from other_functions.finanse_01 import avg_income
 
 our_clients = {}  # słownik — dict
 # słownik = { klucz: wartość, klucz1: wartość, klucz2: wartość }
@@ -10,6 +11,12 @@ while True:
         break
 
     our_clients[client_name] = read_client_data(client_name)
+    print(f"{client_name} data: {our_clients[client_name]}")
+    tmp_data = our_clients[client_name]
+    income = tmp_data["payments"]
+    print(f"Income: {income}")
+    tmp_data["next_payment"] = avg_income(income)
+    print(f"Values after: {tmp_data}")
 
 # koniec pętli while
 print(f"Our full database: {our_clients}")
