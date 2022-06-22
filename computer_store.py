1c6324709d012275706a15a21da3a1bfe44331ca
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-06-22 14:49:18 +0200

    import klasy

---
+++ computer_store.py
@@ -0,0 +1,7 @@
+from store.warehause import Good
+
+our_store = {
+    "shelf": [Good("AT", "i7", 2200, 2020),
+              Good("AT", "i7Mobile", 1800, 2017),
+              Good("XT", "8086", 7, 1994)]
+}
--- store/warehause.py
+++ store/warehause.py
@@ -29,23 +29,28 @@ class Good:
         print(f"Was {3400 / self.speed} time slower than today 3400 MHz")
 
 
-computer_X = Good  # w ten sposób raczej NIE ROBIMY
-computer_0 = Good("Komputer XT", "8086", 4, 1990)
-computer_1 = Good("Komputer AT", "286", 12, 1993)
-computer_2 = Good("Komputer XT/AT", "Pentium/586", 133, 1998)  # poprawne wywołanie tworzenia obiektu
-computer_4 = Good("Komputer Adama", "Core i7", 3200, 2020)
+############ [ koniec definicji klasy ]#######################
 
-computer_1.model = "PC XT"
-computer_1.__serial = "HH76"
-computer_1.set_sn("QQ88")
-computer_2.set_model("PC AT Computer")
 
-computer_0.opis()
-computer_1.opis()
-computer_2.opis()
+if __name__ == "__main__":
+    # dalszy skrypt
+    computer_X = Good  # w ten sposób raczej NIE ROBIMY
+    computer_0 = Good("Komputer XT", "8086", 4, 1990)
+    computer_1 = Good("Komputer AT", "286", 12, 1993)
+    computer_2 = Good("Komputer XT/AT", "Pentium/586", 133, 1998)  # poprawne wywołanie tworzenia obiektu
+    computer_4 = Good("Komputer Adama", "Core i7", 3200, 2020)
 
-# for comp in (computer_0, computer_1, computer_2, computer_4):
-#     comp.history()
+    computer_1.model = "PC XT"
+    computer_1.__serial = "HH76"
+    computer_1.set_sn("QQ88")
+    computer_2.set_model("PC AT Computer")
 
-# print(dir(computer_0))
-# print(dir(computer_1))
+    computer_0.opis()
+    computer_1.opis()
+    computer_2.opis()
+
+    # for comp in (computer_0, computer_1, computer_2, computer_4):
+    #     comp.history()
+
+    # print(dir(computer_0))
+    # print(dir(computer_1))
