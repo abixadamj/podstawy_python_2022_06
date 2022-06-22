c063f951f1c67901aa107f37110713b3e22b2310
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-06-22 11:05:39 +0200

    Markdown - 02

--- md_creation/simple_md.py
+++ md_creation/simple_md.py
@@ -2,3 +2,20 @@
 Prosty skrypt generujący plik w formacie Markdown
 https://www.markdownguide.org/basic-syntax/
 """
+
+import snakemd
+
+doc = snakemd.new_doc("simple_md_file")
+
+doc.add_header("To będzie tytuł", 3)
+doc.add_paragraph("Dzisiaj tworzymy dokument w MD")
+doc.add_horizontal_rule()
+my_par = """
+To jest długi tekst,
+może mieć wiele linijek....
+
+I jest identyczny z tym, co piszemy ;-)
+"""
+doc.add_paragraph(my_par)
+
+doc.output_page()
